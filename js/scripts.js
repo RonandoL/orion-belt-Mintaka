dbfc5818ebb458293f350849aa81a2c4ec8414b3
Ronando Long <ronando.long@gmail.com>
2015-11-03 11:11:12 -0800

    Initial commit. JQ hide/show links.

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  $(".click-rendez").click(function() {
+    $(".rendezvous-hidden").toggle();
+    $(".rendezvous-shown").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".click-mission").click(function() {
+    $(".mission-hidden").toggle();
+    $(".mission-shown").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".click-liftoff").click(function() {
+    $(".liftoff-hidden").toggle();
+    $(".liftoff-shown").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".click-transit").click(function() {
+    $(".transit-hidden").toggle();
+    $(".transit-shown").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".click-link").click(function() {
+    $(".link-hidden").toggle();
+    $(".link-shown").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".click-threat").click(function() {
+    $(".threat-hidden").toggle();
+    $(".threat-shown").toggle();
+  });
+});
